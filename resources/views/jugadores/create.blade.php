ea5d429d2647c845a1542998c888faebfd959985
Diego Nunez <diego.nm97@hotmail.com>
2020-07-01 11:34:14 -0500

    Commit 1.1

--- resources/views/jugadores/create.blade.php
+++ resources/views/jugadores/create.blade.php
@@ -1,7 +1,9 @@
 <head>
+        <link rel="shortcut icon" href="https://image.flaticon.com/icons/svg/3144/3144771.svg">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+        <title>Casino</title>
 </head>
 <body>
 <div class="col-sm-1">
